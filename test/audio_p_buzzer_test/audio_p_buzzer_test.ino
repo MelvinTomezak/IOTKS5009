d67746dadfee18132a99d7d5d7217d478b2d4099
TOMEZAK <melvin.tomezak@capgemini.com>
2024-12-16 15:22:10 +0100

    Mise à jour du test librairie plus utiliser

--- test/audio_p_buzzer_test/audio_p_buzzer_test.ino
+++ test/audio_p_buzzer_test/audio_p_buzzer_test.ino
@@ -1,40 +1,51 @@
-#include <ESP32Tone.h>
-
-// On définit le buzzer sur la broche 25
-#define buzzer_pin 25
+// Modification du code de test car la librarie ESP32TONE.h n'est plus prise en charge aujourd'hui.
+#define BUZZER_PIN 25 
+#define BUZZER_CHANNEL 0  
+#define BUZZER_RESOLUTION 8  
+#define BUZZER_FREQUENCY 0  
 
 void setup() {
-pinMode(buzzer_pin, OUTPUT);
-birthday();
+  ledcSetup(BUZZER_CHANNEL, BUZZER_FREQUENCY, BUZZER_RESOLUTION);
+  ledcAttachPin(BUZZER_PIN, BUZZER_CHANNEL);
+
+// Appel de la fonction pour jouer la Musique d'anniversaire
+  playBirthdaySong();
 }
 
 void loop() {
-
 }
 
-//On génére des sons avec certaines fréquences pour faire la chanson Happy Birthday
-void birthday()
-{
-tone(buzzer_pin,294,250,0);  //The four parameters are pin, frequency, delay and channel 
-tone(buzzer_pin,440,250,0);
-tone(buzzer_pin,392,250,0);
-tone(buzzer_pin,532,250,0);
-tone(buzzer_pin,494,250,0);
-tone(buzzer_pin,392,250,0);
-tone(buzzer_pin,440,250,0);
-tone(buzzer_pin,392,250,0);
-tone(buzzer_pin,587,250,0);
-tone(buzzer_pin,532,250,0);
-tone(buzzer_pin,392,250,0);
-tone(buzzer_pin,784,250,0);
-tone(buzzer_pin,659,250,0);
-tone(buzzer_pin,532,250,0);
-tone(buzzer_pin,494,250,0);
-tone(buzzer_pin,440,250,0);
-tone(buzzer_pin,698,250,0);
-tone(buzzer_pin,659,250,0);
-tone(buzzer_pin,532,250,0);
-tone(buzzer_pin,587,250,0);
-tone(buzzer_pin,532,500,0);
-noTone(buzzer_pin,0);  //Close
+// Fonction permettant de jouer un son sur une frequence
+void playTone(int frequency, int duration) {
+  if (frequency > 0) {
+    ledcWriteTone(BUZZER_CHANNEL, frequency);  
+    delay(duration); 
+  }
+  ledcWriteTone(BUZZER_CHANNEL, 0);  
+  delay(50); 
+}
+
+//Fonction permettant de jouer la musique Happy Birthday (joyeux anniversaire en Fr)
+void playBirthdaySong() {
+  playTone(294, 250);  
+  playTone(440, 250);  
+  playTone(392, 250);  
+  playTone(523, 250);  
+  playTone(494, 250);  
+  playTone(392, 250); 
+  playTone(440, 250); 
+  playTone(392, 250);  
+  playTone(587, 250); 
+  playTone(523, 250);  
+  playTone(392, 250); 
+  playTone(784, 250);  
+  playTone(659, 250);  
+  playTone(523, 250);  
+  playTone(494, 250); 
+  playTone(440, 250);
+  playTone(698, 250);  
+  playTone(659, 250);  
+  playTone(523, 250);  
+  playTone(587, 250);  
+  playTone(523, 500);  
 }

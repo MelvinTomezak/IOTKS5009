c48362802dfb0b3310b0a82953e5e014ed27149d
fsaadaoui <fayrouz.saadaoui@capgemini.com>
2024-12-16 16:07:13 +0100

    Control fan test

--- test/sketch_control_fan_test/sketch_control_fan_test.ino
+++ test/sketch_control_fan_test/sketch_control_fan_test.ino
@@ -1,23 +1,29 @@
-#include <analogWrite.h>
 #define fanPin1 19
 #define fanPin2 18
 
 void setup() {
-pinMode(fanPin1, OUTPUT);
-pinMode(fanPin2, OUTPUT);
+  pinMode(fanPin1, OUTPUT); // Configure la broche fanPin1 comme sortie
+  pinMode(fanPin2, OUTPUT); // Configure la broche fanPin2 comme sortie
 }
 
 void loop() {
-digitalWrite(fanPin1, LOW); //pwm = 0
-analogWrite(fanPin2, 180);
-delay(3000);
-digitalWrite(fanPin1, LOW);
-digitalWrite(fanPin2, LOW);
-delay(1000);
-digitalWrite(fanPin1, HIGH); //pwm = 255
-analogWrite(fanPin2, 210);
-delay(3000);
-digitalWrite(fanPin1, LOW);
-digitalWrite(fanPin2, LOW);
-delay(1000);
+  // Étape 1 : FanPin1 LOW (PWM 0 sur fanPin2)
+  digitalWrite(fanPin1, LOW); 
+  analogWrite(fanPin2, 0); // Signal PWM à 0
+  delay(3000); // Pause de 3 secondes
+  
+  // Étape 2 : Désactivation
+  digitalWrite(fanPin1, LOW);
+  digitalWrite(fanPin2, LOW); // Éteindre complètement
+  delay(1000); // Pause de 1 seconde
+  
+  // Étape 3 : FanPin1 HIGH (PWM 255 sur fanPin2)
+  digitalWrite(fanPin1, HIGH); 
+  analogWrite(fanPin2, 255); // Signal PWM à 255 (pleine puissance)
+  delay(3000); // Pause de 3 secondes
+  
+  // Étape 4 : Désactivation
+  digitalWrite(fanPin1, LOW);
+  digitalWrite(fanPin2, LOW); // Éteindre complètement
+  delay(1000); // Pause de 1 seconde
 }

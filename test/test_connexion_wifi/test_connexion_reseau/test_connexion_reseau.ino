9808de5533e1d5f6ad4a305df91028be64a484d0
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-12-17 10:52:13 +0100

    [UPDATE] test connexion reseau

--- test/test_connexion_wifi/test_connexion_reseau/test_connexion_reseau.ino
+++ test/test_connexion_wifi/test_connexion_reseau/test_connexion_reseau.ino
@@ -1,43 +1,36 @@
-//Test de connexion à un réseau
 #include <WiFi.h>
-#include <LiquidCrystal_I2C.h>
-
+ 
+// Identifiants du réseau Wi-Fi
 const char* ssid = "Fayrouz";
 const char* password = "123sf3_8";
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-void setup(){
-    lcd.begin();
-    lcd.backlight();
-    Serial.begin(115200);
-    delay(1000);
-
-    WiFi.mode(WIFI_STA); 
-    WiFi.begin(ssid, password);
-    Serial.println("\nConnecting");
-
-    unsigned long startAttemptTime = millis();  
-
-    while(WiFi.status() != WL_CONNECTED){
-        Serial.print(".");
-        delay(100);
-// Si après les dix secondes on est pas connecté on affiche wifi non connecte
-         if (millis() - startAttemptTime >= 10000) {
-            lcd.clear();  
-            lcd.setCursor(0, 0);
-            lcd.print("WiFi non connecte");
-            return;  
-        }
+ 
+void setup() {
+  Serial.begin(115200); // Initialisation de la communication série
+ 
+  // Connexion au réseau Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnexion au Wi-Fi...");
+ 
+  unsigned long startAttemptTime = millis();  
+ 
+  // Tentative de connexion pendant 10 secondes
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print("."); // Affichage d'un point pour chaque tentative
+ 
+    // Si après 10 secondes, la connexion échoue
+    if (millis() - startAttemptTime >= 10000) {
+      Serial.println("\nEchec de la connexion.");
+      return; // Fin de la fonction si non connecté
     }
-    
-    lcd.clear(); 
-    lcd.setCursor(0, 0);
-    lcd.print("WiFi connecte");
-    lcd.setCursor(0, 1); 
-    lcd.print("IP: ");
-    lcd.print(WiFi.localIP());
+  }
+ 
+  // Si la connexion réussit
+  Serial.println("\nConnecté au réseau Wi-Fi !");
+  Serial.print("Adresse IP : ");
+  Serial.println(WiFi.localIP());  // Affiche l'adresse IP locale de l'ESP32
 }
-
-void loop(){
-  if ()
+ 
+void loop() {
+  // Code à exécuter en boucle (si nécessaire)
 }

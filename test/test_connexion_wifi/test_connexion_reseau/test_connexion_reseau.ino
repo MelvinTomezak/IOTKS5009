82e642d5988429703359bfb660f4d8ee427d58b7
MelvinTomezak <melvin.tomezak@etu.univ-amu.fr>
2024-12-17 10:19:21 +0100

    test connexion wifi avec affichage ecran

--- test/test_connexion_wifi/test_connexion_reseau/test_connexion_reseau.ino
+++ test/test_connexion_wifi/test_connexion_reseau/test_connexion_reseau.ino
@@ -1,10 +1,14 @@
 //Test de connexion à un réseau
 #include <WiFi.h>
+#include <LiquidCrystal_I2C.h>
 
 const char* ssid = "Fayrouz";
 const char* password = "123sf3_8";
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup(){
+    lcd.begin();
+    lcd.backlight();
     Serial.begin(115200);
     delay(1000);
 
@@ -17,18 +21,23 @@ void setup(){
     while(WiFi.status() != WL_CONNECTED){
         Serial.print(".");
         delay(100);
-
-        // Si on attend plus de 10 secondes alors fin
-        if (millis() - startAttemptTime >= 10000) {
-            Serial.println("\nFailed to connect to WiFi. Check credentials or signal.");
+// Si après les dix secondes on est pas connecté on affiche wifi non connecte
+         if (millis() - startAttemptTime >= 10000) {
+            lcd.clear();  
+            lcd.setCursor(0, 0);
+            lcd.print("WiFi non connecte");
             return;  
         }
     }
-
-    Serial.println("\nConnected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
+    
+    lcd.clear(); 
+    lcd.setCursor(0, 0);
+    lcd.print("WiFi connecte");
+    lcd.setCursor(0, 1); 
+    lcd.print("IP: ");
+    lcd.print(WiFi.localIP());
 }
 
 void loop(){
+  if ()
 }
